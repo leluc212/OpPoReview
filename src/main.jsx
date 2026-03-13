563a2e8c5f55eae64cef2f1ca8fdb4a2c4420279
leluc2k4 <lucltse184288@fpt.edu.vn>
2026-03-13 11:51:35 +0700

    Fix: Persist Cognito session on F5 refresh - Import amplifyClient in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-// Amplify v6 will be configured in amplifyClient.js when first imported
-// No need to configure here to avoid double configuration
+// Import amplifyClient to ensure Amplify is configured before app starts
+import './utils/amplifyClient';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
